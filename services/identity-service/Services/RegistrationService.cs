211194ace67fcfe507d133223546d6c5b6f9f466
vindyawijerathna1-eng <vindyawijerathna1@gmail.com>
2026-08-19 22:21:33 +0530

    feat(CEYQ-9): add registration service

---
+++ services/identity-service/Services/RegistrationService.cs
@@ -0,0 +1,36 @@
+using IdentityService.DTOs;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService.Services;
+
+public class RegistrationService
+{
+    private readonly PasswordHasher<User> _passwordHasher;
+
+    public RegistrationService()
+    {
+        _passwordHasher = new PasswordHasher<User>();
+    }
+
+    public User Register(RegisterRequest request)
+    {
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            PhoneNumber = request.PhoneNumber,
+            Nationality = request.Nationality,
+            Role = UserRole.Visitor
+        };
+
+        user.PasswordHash = _passwordHasher.HashPassword(
+            user,
+            request.Password
+        );
+
+        return user;
+    }
+}
