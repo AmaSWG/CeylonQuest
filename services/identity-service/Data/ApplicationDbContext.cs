4a47c53f113ac8ea2f8e5e09d677433cb96ac866
vindyawijerathna1-eng <vindyawijerathna1@gmail.com>
2026-08-21 01:39:00 +0530

    feat(CEYQ-9): enforce unique registration emails

--- services/identity-service/Data/ApplicationDbContext.cs
+++ services/identity-service/Data/ApplicationDbContext.cs
@@ -11,4 +11,13 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+    }
 }
